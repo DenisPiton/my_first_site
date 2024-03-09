6decc21e0a852b045dc0ccc3d5dd055f925ba003
Denis <denfill008@mail.ru>
2024-03-09 15:12:44 +0300

    init3

--- my_test/my_test/settings.py
+++ my_test/my_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(_kpt4ak#vnu0x_r5%_4q!-vics@9n(tyb*(ja7e++6ebq+wwy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ideni.pythonanywhere.com"]
+ALLOWED_HOSTS = ["ideni.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
